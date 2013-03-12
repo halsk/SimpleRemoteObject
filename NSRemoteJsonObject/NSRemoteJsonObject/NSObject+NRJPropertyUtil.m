8033fe07180619ea23526cdc8fdfb588e0af8ee0
Hal Seki <hal@plants-web.jp>
2013-03-12 23:43:36 +0900

    added AFNetworking

---
+++ NSRemoteJsonObject/NSRemoteJsonObject/NSObject+NRJPropertyUtil.m
@@ -0,0 +1,13 @@
+//
+//  NRJPropertyUtil.m
+//  NSRemoteJsonObject
+//
+//  Created by Haruyuki Seki on 3/12/13.
+//  Copyright (c) 2013 Georepublic. All rights reserved.
+//
+
+#import "NRJPropertyUtil.h"
+
+@implementation NRJPropertyUtil
+
+@end
---
+++ NSRemoteJsonObject/NSRemoteJsonObjectTests/classes/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  NSRemoteJsonObject
+//
+//  Created by Haruyuki Seki on 3/12/13.
+//  Copyright (c) 2013 Georepublic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
