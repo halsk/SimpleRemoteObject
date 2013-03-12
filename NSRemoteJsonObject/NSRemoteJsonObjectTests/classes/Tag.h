1e7de292a23b9cf385b429d962889ab300d3a06e
Hal Seki <hal@plants-web.jp>
2013-03-13 00:48:27 +0900

    added test server

---
+++ NSRemoteJsonObject/NSRemoteJsonObjectTests/classes/Tag.h
@@ -0,0 +1,13 @@
+//
+//  Tag.h
+//  NSRemoteJsonObject
+//
+//  Created by Haruyuki Seki on 3/13/13.
+//  Copyright (c) 2013 Georepublic. All rights reserved.
+//
+
+#import "NSRemoteJsonObject.h"
+
+@interface Tag : NSRemoteJsonObject
+
+@end
---
+++ NSRemoteJsonObject/NSRemoteJsonObjectTests/classes/Tag.m
@@ -0,0 +1,13 @@
+//
+//  Tag.m
+//  NSRemoteJsonObject
+//
+//  Created by Haruyuki Seki on 3/13/13.
+//  Copyright (c) 2013 Georepublic. All rights reserved.
+//
+
+#import "Tag.h"
+
+@implementation Tag
+
+@end
