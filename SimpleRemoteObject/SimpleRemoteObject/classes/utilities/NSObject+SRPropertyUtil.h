78cca1112bdcce9eae5518124118ec037ce28843
Hal Seki <hal@plants-web.jp>
2013-03-13 11:16:28 +0900

    created new project

---
+++ SimpleRemoteObject/SimpleRemoteObject/classes/utilities/NSObject+SRPropertyUtil.h
@@ -0,0 +1,14 @@
+//
+//  NSObject+NRJPropertyUtil.h
+//  NSRemoteJsonObject
+//
+//  Created by Hal Seki on 3/12/13.
+//  Copyright (c) 2013 Georepublic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject(SRPropertyUtil)
++ (NSDictionary *)properties;
+
+@end
