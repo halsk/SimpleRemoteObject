6e3b13ddaf6d1accbaf0bdbbd150b6797a3be958
Hal Seki <hal@plants-web.jp>
2013-03-13 11:00:23 +0900

    changed test case to Pod

--- SimpleRemoteObject/SimpleRemoteObjectTests/SimpleRemoteObjectTests.m
+++ SimpleRemoteObject/SimpleRemoteObjectTests/SimpleRemoteObjectTests.m
@@ -1,32 +1,73 @@
-//
-//  SimpleRemoteObjectTests.m
-//  SimpleRemoteObjectTests
-//
-//  Created by Haruyuki Seki on 3/13/13.
-//  Copyright (c) 2013 Georepublic. All rights reserved.
-//
 
-#import "SimpleRemoteObjectTests.h"
+#import "Kiwi.h"
+#import "User.h"
 
-@implementation SimpleRemoteObjectTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
-}
+#import "NSObject+NRJPropertyUtil.h"
+#import "NRJRemoteConfig.h"
+#import "NRJRemoteJsonObject.h"
+#import "Tag.h"
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
-}
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in SimpleRemoteObjectTests");
-}
+SPEC_BEGIN(PropertyUtil)
 
-@end
+describe(@"PropertyUtil", ^{
+    it(@"should have 3 properties", ^{
+        [[[User properties] should] haveCountOf:3];
+    });
+    it(@"should have name , mail and age", ^{
+        [[[[User properties] allKeys] should] contain:@"name"];
+        [[[[User properties] allKeys] should] contain:@"mail"];
+        [[[[User properties] allKeys] should] contain:@"age"];
+    });
+    it(@"should return object type ", ^{
+        [[[[User properties] objectForKey:@"name"] should] equal:@"NSString"];
+        [[[[User properties] objectForKey:@"mail"] should] equal:@"NSString"];
+        [[[[User properties] objectForKey:@"age"] should] equal:@"i"];
+    });
+});
+
+SPEC_END
+
+SPEC_BEGIN(RemoteConfig)
+
+describe(@"RemoteConfig", ^{
+    context(@"read remote object", ^{
+        beforeAll(^{
+            [NRJRemoteConfig defaultConfig].baseurl = @"http://localhost:2000/";
+        });
+        
+        it(@"should have api endpoint", ^{
+            [[[NRJRemoteConfig defaultConfig].baseurl should] equal:@"http://localhost:2000/"];
+        });
+    });
+});
+
+SPEC_END
+
+SPEC_BEGIN(RemoteObject)
+
+describe(@"RemoteConfig", ^{
+    context(@"read remote tag object", ^{
+        beforeAll(^{
+            [NRJRemoteConfig defaultConfig].baseurl = @"http://localhost:2000/";
+        });
+        
+        it(@"should read remote json", ^{
+            __block NSArray *ret;
+            [Tag fetchAsync:^(NSArray *allRemote, NSError *error) {
+                ret = allRemote;
+            }];
+            [[expectFutureValue(ret) shouldEventually] beNonNil];
+            [[expectFutureValue(ret) shouldEventually] haveCountOf:3];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:0]).name) shouldEventually] equal:@"カフェ"];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:1]).name) shouldEventually] equal:@"ハッカソン"];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:2]).name) shouldEventually] equal:@"破滅"];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:0]).resource_uri) shouldEventually] equal:@"/api/tag/%E3%82%AB%E3%83%95%E3%82%A7"];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:0]).slug) shouldEventually] equal:@"カフェ"];
+            [[expectFutureValue(((Tag *)[ret objectAtIndex:0]).remoteId) shouldEventually] equal:theValue(2)];
+        });
+    });
+});
+
+SPEC_END
